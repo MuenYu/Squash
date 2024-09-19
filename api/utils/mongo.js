a384d55974230085fc66ac0696a71e699cd03fea
MuenYu <parasomnia97@gmail.com>
2024-09-19 11:10:26 +0000

    configure rds connection

--- api/utils/mongo.js
+++ api/utils/mongo.js
@@ -1,11 +1,45 @@
 import mongoose from "mongoose";
 import { getSecret } from "./secretmanager.js";
-import { downloadCert } from "./cert.js";
+import fetch from 'node-fetch';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
 
 const key = "mongodb";
+const certUrl = "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem";
 
 export async function connectMongo() {
     await downloadCert();
     const url = await getSecret(key);
     await mongoose.connect(url);
+    console.log(`mongodb init success`)
+}
+
+async function downloadCert() {
+    const filePath = join(process.cwd(), 'global-bundle.pem'); // Save in the root of the project
+
+    const response = await fetch(certUrl);
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch certificate: ${response.statusText}`);
+    }
+
+    return new Promise((resolve,reject) => {
+        // Create a write stream to save the file
+        const fileStream = createWriteStream(filePath);
+
+        // Pipe the downloaded file into the stream
+        response.body.pipe(fileStream);
+
+        // Return a promise that resolves when the file has finished writing
+        fileStream.on('finish', () => {
+            console.log('Certificate downloaded successfully:', filePath);
+            resolve();
+        });
+
+        fileStream.on('error', (err) => {
+            console.error('Error writing certificate file:', err);
+            reject(err);
+        });
+    })
+
 }
---
+++ api/utils/rds.js
@@ -0,0 +1,41 @@
+import { getSecret } from "./secretmanager.js"
+import knex from "knex";
+
+const host = await getSecret("rdsHost")
+const user = await getSecret("rdsUser")
+const password = await getSecret("rdsPass")
+const database = await getSecret("rdsDB")
+
+export const mysql = knex({
+    client: 'mysql2', // Use 'mysql2' as the client for MySQL connections
+    connection: {
+        host: host,        // Replace with your DB host
+        user: user,             // Replace with your DB username
+        password: password,     // Replace with your DB password
+        database: database,  // Replace with your DB name
+        multipleStatements: true
+    },
+    pool: { min: 0, max: 10 }, // Optional: Pooling options for managing connections
+});
+
+export async function initRDS() {
+    if (mysql) {
+        await mysql.raw(initSQL);
+        console.log('rds init success');
+    } else {
+        throw new Error("mysql init failed: no connection established")
+    }
+}
+
+const initSQL = `
+USE ${database};
+
+CREATE TABLE IF NOT EXISTS history (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    original_name VARCHAR(255) NOT NULL,
+    owner VARCHAR(255) NOT NULL, 
+    compression_level VARCHAR(255), 
+    file_name VARCHAR(255) NOT NULL,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`
