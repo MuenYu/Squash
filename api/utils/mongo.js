4a7024c5b737c4ffeb2576e219aeef628752a284
MuenYu <parasomnia97@gmail.com>
2024-09-22 05:58:22 +0000

    use axios instead of node-fetch

--- api/utils/mongo.js
+++ api/utils/mongo.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { getSecret } from "./secretmanager.js";
-import fetch from 'node-fetch';
-import { createWriteStream } from 'fs';
+import axios from "axios"
+import { writeFile } from 'fs/promises';
 import { join } from 'path';
 import Video from "../model/video.js";
 
@@ -18,29 +18,21 @@ export async function connectMongo() {
 async function downloadCert() {
     const filePath = join(process.cwd(), 'global-bundle.pem'); // Save in the root of the project
 
-    const response = await fetch(certUrl);
+    const response = await axios.get(certUrl);
 
-    if (!response.ok) {
+    if (response.status !== 200) {
         throw new Error(`Failed to fetch certificate: ${response.statusText}`);
     }
 
-    return new Promise((resolve,reject) => {
-        // Create a write stream to save the file
-        const fileStream = createWriteStream(filePath);
-
-        // Pipe the downloaded file into the stream
-        response.body.pipe(fileStream);
-
-        // Return a promise that resolves when the file has finished writing
-        fileStream.on('finish', () => {
-            console.log('Certificate downloaded successfully:', filePath);
-            resolve();
-        });
-
-        fileStream.on('error', (err) => {
-            console.error('Error writing certificate file:', err);
-            reject(err);
-        });
+    return new Promise((resolve, reject) => {
+        writeFile(filePath, response.data)
+            .then(() => {
+                console.log('Certificate downloaded successfully:', filePath);
+                resolve();
+            }).catch((err) => {
+                console.error('Error writing certificate file:', err);
+                reject(err);
+            })
     })
 }
 
