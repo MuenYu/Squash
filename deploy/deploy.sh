2785f81bb2b73a3a932f04ebc2c57eeae7c96fed
MuenYu <parasomnia97@gmail.com>
2024-09-26 05:53:41 +0000

    update dependencies and update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,17 @@
-cd ../web
+# install dep
+cd ~/CAB432/api
+npm i
+
+# build the webpage
+cd ~/CAB432/web
+npm i
 npm run build
-mv ./dist ../api/public
+mv -f ./dist ../api/public
+
+# systemd
+cd ~/CAB432/deploy
+sudo cp ./squash.service /etc/systemd/system/squash.service
+sudo systemctl daemon-reload
+sudo systemctl enable squash
+sudo systemctl start squash
+sudo systemctl status squash
