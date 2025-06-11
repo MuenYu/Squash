0cfd560a1a0402c6055fc8f09a6687ba93e5222a
MuenYu <parasomnia97@gmail.com>
2025-06-12 00:11:28 +1000

    static refactor: not-found

---
+++ web-next/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <h1 className="text-5xl font-bold mb-5">404</h1>
+      <p className="text-xl">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
